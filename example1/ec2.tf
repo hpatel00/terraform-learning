0d8a04d829f4375ccc1b48baca05e8d48ea13154
hpatel00 <hpatel2356@gmail.com>
2022-04-23 20:43:27 -0400

    fixed cidr_block for ingress in ec2

--- example1/ec2.tf
+++ example1/ec2.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "sample" {
-  ami = "ami-0bb6af715826253bf"
-  instance_type = "t3.micro"
-  security_groups = [aws_security_group.allow_tls.id]
+  ami                    = "ami-0bb6af715826253bf"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
 
 output "public_ip" {
