b7c14c62ebf21ba83e081a5c06ec8d035d64d946
hpatel00 <hpatel2356@gmail.com>
2022-04-23 21:59:23 -0400

    readded provisioner into ec2 file in example2

--- example2/ec2/main.tf
+++ example2/ec2/main.tf
@@ -2,26 +2,29 @@ resource "aws_instance" "sample" {
   ami = data.aws_ami.example.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = [var.sg]
+}
+
 #  provisioner "local-exec" {
 #    command = <<EOF
 #sleep 60
 #cd /home/centos/ansible
-#ansible-playbook -i ${self.public_ip}, roboshop.yml -e HOST=all -e role_name=frontend -e ENV=dev -e ansible_user=centos -e ansible_password=DevOps321
+#ansible-playbook -i ${self.public_ip}, roboshop.yml -e HOST=all -e role_name=frontend -e ENV=dev -e ansible_user=centos -e ansible_password=DevOps321 -e APP_VERSION=1.0.0
 #EOF
 #
 #  }
 
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = DevOps321
-      host     = self.public_ip
-    }
-    inline = [
-      "ansible-pull -U https://github.com/hpatel00/ansible roboshop.yml -e HOST=localhost -e role_name=frontend -e ENV=dev"
-    ]
+
+
+provisioner "remote-exec" {
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = DevOps321
+    host     = self.public_ip
   }
+  inline = [
+    "ansible-pull -U https://github.com/hpatel00/ansible roboshop.yml -e HOST=localhost -e role_name=frontend -e ENV=dev -e APP_VERSION=1.0.0"
+  ]
 }
 
 variable "sg" {}
